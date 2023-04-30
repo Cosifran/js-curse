dd886f53d614479c523f8ad831755c88a74dab79
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2023-04-30 14:19:31 -0500

    creacion de la carpeta ejercicio lista

---
+++ primer-proyecto-nodejs/ejercicio-lista/ejercicio-lista.js
@@ -0,0 +1,7 @@
+lista = ["Francisco Javier", 21, true, fecha = new Date(2002, 1, 4), 
+book ={
+    titulo: "inccidente del perro a media noche",
+    autor: "Mark Haddon",
+    fecha: "Mark Haddon",
+    url: "https://www.casadellibro.com.co/libro-el-curioso-incidente-del-perro-a-medianoche/9788478889105/981512"
+} ];
