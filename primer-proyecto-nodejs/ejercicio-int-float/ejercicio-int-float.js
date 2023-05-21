10a5e805d4921b350e437cd24e34b7595a774fc9
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2023-05-21 18:42:47 -0500

    ejercicio de redondeo

---
+++ primer-proyecto-nodejs/ejercicio-int-float/ejercicio-int-float.js
@@ -0,0 +1,8 @@
+let alturaInt = 180;
+let alturaFloat = 1.80;
+let pesoFloat = 58;
+let maximoValor = Number.MAX_VALUE + 1;
+
+console.log(alturaFloat.toPrecision(1));
+console.log(pesoFloat.toPrecision(2));
+console.log(Number.MIN_VALUE == maximoValor);
