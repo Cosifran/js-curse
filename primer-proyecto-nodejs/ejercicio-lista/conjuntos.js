3c40ad207fb6eba1ae7debfd68112627f0473bcb
Francisco Front <90176645+Cosifran@users.noreply.github.com>
2023-06-17 09:43:27 -0500

    Create conjuntos.js

---
+++ primer-proyecto-nodejs/ejercicio-lista/conjuntos.js
@@ -0,0 +1,16 @@
+//ejercicios con set 
+
+let nombreFamilia = new Set();
+
+ nombreFamilia.add("Francisco");
+ nombreFamilia.add("Rosa");
+ nombreFamilia.add("Sareth");
+ nombreFamilia.add("Benjamin");
+ nombreFamilia.add("Juliana");
+ nombreFamilia.add("Francisco");
+
+console.log(nombreFamilia);
+
+nombreFamilia.add("Javascript");
+
+console.log(nombreFamilia);
