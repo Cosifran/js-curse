1b8145e241d4119a42e22483add8ef3a43638eb9
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2023-07-07 23:33:24 -0500

    Terminate exercise maps google

--- primer-proyecto-nodejs/ejercicio-maps/index.js
+++ primer-proyecto-nodejs/ejercicio-maps/index.js
@@ -1,21 +1,45 @@
-let map;
+let marker, map;
 
-async function initMap() {
-    const position = { lat: -25.344, lng: 131.031 };
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+function initMap() {
+    // console.log("Inicializando mapa")
+    const posicion = {
+        lat: -25.363,
+        lng: 131.044
+    }
 
-  map = new Map(document.getElementById("map"), {
-    center: position,
-    zoom: 4,
-  });
+    const posicion2 = {
+      lat: 11.004695979994754,
+      lng: -74.8052165689702
+  }
 
-  const marker = new AdvancedMarkerView({
-    map: map,
-    position: position,
-    title: "Uluru",
-  });
+  const posicion3 = {
+    lat: 52.91583414923662,
+    lng: 18.616401631184274
+  }
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 2,
+        center: posicion
+    })
+
+    marker = new google.maps.Marker({
+        position: posicion,
+        map,
+        title: "Australia"
+    });
+
+    marker2 = new google.maps.Marker({
+      position:posicion2,
+      map,
+      title: "Barranquilla"
+    })
+
+    marker3 = new google.maps.Marker({
+      position:posicion3,
+      map,
+      title: "Polonia"
+    })
 }
 
-initMap();
+
+
