224e0546c3cc5cc852e0410843d4eff298c2f82c
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2023-07-14 17:59:44 -0500

    14/07/23 update

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,8 +1,13 @@
+import React from 'react'
 import Header from "./components/Header";
+import Flooter from "./components/Flooter";
 function App() {
   return (
     <div className="App">
-      < Header />
+      < Header
+      title = "Welcome to our website"
+      />
+      < Flooter />
     </div>
   );
 }
---
+++ React/my-app/src/components/Flooter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = (title) => {
+    console.log(title)
+    return (
+        <footer>
+            <p>Todo los derechos recervados &copy; </p>
+        </footer>
+     );
+}
+ 
+export default Footer;
